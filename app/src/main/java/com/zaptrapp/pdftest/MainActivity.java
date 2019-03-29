876cbd1ee27bfb5c0679457229d26d9ccaa29b7c
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2019-03-29 09:19:52 +0530

    Initial commit

---
+++ app/src/main/java/com/zaptrapp/pdftest/MainActivity.java
@@ -0,0 +1,119 @@
+package com.zaptrapp.pdftest;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.CMYKColor;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initpdf();
+    }
+
+    private void openPDF(String filename){
+        Log.d("open", "openPDF: ");
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +"/"+ filename);
+        Intent target = new Intent(Intent.ACTION_VIEW);
+        target.setDataAndType(Uri.fromFile(file),"application/pdf");
+        target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        Intent intent = Intent.createChooser(target, "Open File");
+
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "Install a pdf reader application to open pdf files", Toast.LENGTH_SHORT).show();
+            // Instruct the user to install a PDF reader here, or something
+        }
+    }
+
+    private void initpdf() {
+        // TODO Auto-generated method stub
+        com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+
+        try {
+            String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/vindroid";
+
+
+            File dir = new File(path);
+            if(!dir.exists())
+                dir.mkdirs();
+
+            Log.d("PDFCreator", "PDF Path: " + path);
+
+
+            File file = new File(dir, "sample.pdf");
+            FileOutputStream fOut = new FileOutputStream(file);
+
+            PdfWriter.getInstance(document, fOut);
+
+            //open the document
+            document.open();
+
+
+            Paragraph p1 = new Paragraph("Sample PDF CREATION USING IText");
+            Font paraFont= new Font(Font.FontFamily.COURIER);
+            p1.setAlignment(Paragraph.ALIGN_CENTER);
+            p1.setFont(paraFont);
+
+            //add paragraph to document
+            document.add(p1);
+
+            Paragraph p2 = new Paragraph("This is an example of a simple paragraph");
+            Font paraFont2= new Font(Font.FontFamily.COURIER,14.0f,0, CMYKColor.GREEN);
+            p2.setAlignment(Paragraph.ALIGN_CENTER);
+            p2.setFont(paraFont2);
+
+            document.add(p2);
+
+//            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//            Bitmap bitmap = BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.ic_launcher_foreground);
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100 , stream);
+//            Image myImg = Image.getInstance(stream.toByteArray());
+//            myImg.setAlignment(Image.MIDDLE);
+//
+//            //add image to document
+//            document.add(myImg);
+
+
+
+
+        } catch (DocumentException de) {
+            Log.e("PDFCreator", "DocumentException:" + de);
+        } catch (IOException e) {
+            Log.e("PDFCreator", "ioException:" + e);
+        } finally
+        {
+            document.close();
+            openPDF("/vindroid/sample.pdf");
+        }
+
+
+
+    }
+}
